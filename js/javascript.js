826f114575de4968b07c5bf6e4e2f0585df38ace
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2025-01-10 22:41:01 +0100

    feat: let grid keep same size with different number of divs
    Hardcoded in CSS so far, to figure out how to achive the square of divs
    at all.

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,16 @@
 const containerGridDiv = document.querySelector(".container-grid-div");
+let gridDivNumX = 0;
+let gridDivNumY = 1;
 
 function drawSketch(size = 16) {
-    const gridDivs = new Array(size**2).fill().map(() => {
+    const gridDivs = new Array(size ** 2).fill().map(() => {
         const gridDiv = document.createElement("div");
         gridDiv.setAttribute("class", "grid-div");
+        gridDiv.textContent = `${gridDivNumY}/${++gridDivNumX}`;
+        if (gridDivNumX % size === 0) {
+            gridDivNumY++;
+            gridDivNumX = 0;
+        }
         containerGridDiv.appendChild(gridDiv);
         return gridDiv;
     });
