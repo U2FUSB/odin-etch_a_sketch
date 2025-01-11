57ab3812a29a9a8c962eeac546929d3adc04d909
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2025-01-11 03:07:44 +0100

    feat: Make dynamic grid div number possible

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,19 @@
 const containerGridDiv = document.querySelector(".container-grid-div");
-let gridDivNumX = 0;
-let gridDivNumY = 1;
+let gridDivNum = 1;
 
 function drawSketch(size = 16) {
     const gridDivs = new Array(size ** 2).fill().map(() => {
         const gridDiv = document.createElement("div");
         gridDiv.setAttribute("class", "grid-div");
-        gridDiv.textContent = `${gridDivNumY}/${++gridDivNumX}`;
-        if (gridDivNumX % size === 0) {
-            gridDivNumY++;
-            gridDivNumX = 0;
-        }
         containerGridDiv.appendChild(gridDiv);
         return gridDiv;
     });
-
-    console.log(gridDivs);
+    gridDivs.map((gridDiv) => {
+        gridDiv.textContent = gridDivNum++;
+    });
+    document
+        .querySelector("*")
+        .setAttribute("style", `--grid-div-num: ${size}`);
+    return gridDivs;
 }
-// 	- Divs appear in a square. (maybe wrap?)
-// 	- Fixed size of singe divs? Maybe flexible css unit
-
 drawSketch();
