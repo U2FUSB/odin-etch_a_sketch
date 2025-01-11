885b9c333a574d92977a95203023978fe8836d15
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2025-01-11 03:20:22 +0100

    test: Add console.asserts
    to aid in building input handling later

--- js/javascript.js
+++ js/javascript.js
@@ -17,3 +17,13 @@ function drawSketch(size = 16) {
     return gridDivs;
 }
 drawSketch();
+
+
+// Some assertions for testing
+console.assert(drawSketch().length === 16**2, "Default grid-div number wrong")
+console.assert(drawSketch(13).length === 13**2, "Dynamic grid-div number wrong")
+// console.assert(drawSketch(-13).length === 16**2, "Negative grid-div number does not create default")
+// console.assert(drawSketch(0).length === 16**2, "0 as grid-div number does not create default")
+// console.assert(drawSketch(101).length === 16**2, "Bigger then 100 as grid-div number does not create 100*100 grid")
+// console.assert(drawSketch(0).length === 16**2, "0 as grid-div number does not create default")
+// console.assert(drawSketch("testString").length === 16**2, "String as grid-div number does not create default")
