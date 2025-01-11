1a4908399d51711ffa36d01ebd1b1f250104dd95
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2025-01-11 20:53:37 +0100

    feat: Put gridDiv numbers in separate function

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,4 @@
 const containerGridDiv = document.querySelector(".container-grid-div");
-let gridDivNum = 1;
 
 function drawSketch(size = 16) {
     const gridDivs = new Array(size ** 2).fill().map(() => {
@@ -8,20 +7,35 @@ function drawSketch(size = 16) {
         containerGridDiv.appendChild(gridDiv);
         return gridDiv;
     });
-    gridDivs.map((gridDiv) => {
-        gridDiv.textContent = gridDivNum++;
-    });
+    addGridDivCounter(gridDivs);
     document
         .querySelector("*")
         .setAttribute("style", `--grid-div-num: ${size}`);
-    return gridDivs;
+}
+function addGridDivCounter(gridDivs) {
+    let gridDivNumX = 0;
+    let gridDivNumY = 1;
+    gridDivs.map((gridDiv) => {
+        gridDiv.textContent = `${++gridDivNumX}/${gridDivNumY}`;
+        if (gridDivNumX % (Math.sqrt(gridDivs.length)) === 0) {
+            gridDivNumX = 0;
+            gridDivNumY++;
+        }
+    });
 }
 drawSketch();
 
-
 // Some assertions for testing
-console.assert(drawSketch().length === 16**2, "Default grid-div number wrong")
-console.assert(drawSketch(13).length === 13**2, "Dynamic grid-div number wrong")
+// drawSketch();
+// console.assert(
+//     containerGridDiv.childElementCount === 16 ** 2,
+//     "Default grid-div number wrong"
+// );
+// drawSketch(13)
+// console.assert(
+//     containerGridDiv.childElementCount === 13 ** 2,
+//     "Dynamic grid-div number wrong"
+// );
 // console.assert(drawSketch(-13).length === 16**2, "Negative grid-div number does not create default")
 // console.assert(drawSketch(0).length === 16**2, "0 as grid-div number does not create default")
 // console.assert(drawSketch(101).length === 16**2, "Bigger then 100 as grid-div number does not create 100*100 grid")
