78f87e21e9631e2c87ac7aaf48ed4830ba20cfb9
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2025-01-12 21:29:12 +0100

    feat: Add input handling

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # odin-etch_a_sketch
 Project build, based on instructions from [theOdinProject: Etch-a-Sketch](https://www.theodinproject.com/lessons/foundations-etch-a-sketch)
-
-This text is considered a hotfix
--- js/javascript.js
+++ js/javascript.js
@@ -1,43 +1,41 @@
 const containerGridDiv = document.querySelector(".container-grid-div");
+const chooseNumberButton = document.querySelector(".choose-number-button");
 
 function drawSketch(size = 16) {
+    containerGridDiv.replaceChildren();
     const gridDivs = new Array(size ** 2).fill().map(() => {
         const gridDiv = document.createElement("div");
         gridDiv.setAttribute("class", "grid-div");
         containerGridDiv.appendChild(gridDiv);
         return gridDiv;
     });
-    // addGridDivCounter(gridDivs);
     document
-        .querySelector("*")
-        .setAttribute("style", `--grid-div-num: ${size}`);
+    .querySelector("*")
+    .setAttribute("style", `--grid-div-num: ${size}`);
+    // addGridDivCounter(gridDivs); //If I want to count the squares on the screen
 }
 function addGridDivCounter(gridDivs) {
     let gridDivNumX = 0;
     let gridDivNumY = 1;
     gridDivs.map((gridDiv) => {
         gridDiv.textContent = `${++gridDivNumX}/${gridDivNumY}`;
-        if (gridDivNumX % (Math.sqrt(gridDivs.length)) === 0) {
+        if (gridDivNumX % Math.sqrt(gridDivs.length) === 0) {
             gridDivNumX = 0;
             gridDivNumY++;
         }
     });
 }
+function addListenerGridDivNumber() {
+    chooseNumberButton.addEventListener("click", () => {
+        const rawUserInput = prompt("Enter a number between 1 and 100:", 16);
+        const userInputAsNumber = Number(rawUserInput);
+        const numberOfGridDivs =
+            userInputAsNumber >= 1 && userInputAsNumber <= 100
+                ? Math.floor(userInputAsNumber)
+                : 16;
+        drawSketch(numberOfGridDivs)
+    });
+}
 drawSketch();
+addListenerGridDivNumber()
 
-// Some assertions for testing
-// drawSketch();
-// console.assert(
-//     containerGridDiv.childElementCount === 16 ** 2,
-//     "Default grid-div number wrong"
-// );
-// drawSketch(13)
-// console.assert(
-//     containerGridDiv.childElementCount === 13 ** 2,
-//     "Dynamic grid-div number wrong"
-// );
-// console.assert(drawSketch(-13).length === 16**2, "Negative grid-div number does not create default")
-// console.assert(drawSketch(0).length === 16**2, "0 as grid-div number does not create default")
-// console.assert(drawSketch(101).length === 16**2, "Bigger then 100 as grid-div number does not create 100*100 grid")
-// console.assert(drawSketch(0).length === 16**2, "0 as grid-div number does not create default")
-// console.assert(drawSketch("testString").length === 16**2, "String as grid-div number does not create default")
