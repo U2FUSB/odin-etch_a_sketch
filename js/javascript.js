8bc225480cf39585ffc500b40a011a0f742e31b4
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2025-01-17 18:10:09 +0100

    chore: Push before AFK

--- js/javascript.js
+++ js/javascript.js
@@ -10,9 +10,12 @@ function drawSketch(size = 16) {
         return gridDiv;
     });
     document
-    .querySelector("*")
-    .setAttribute("style", `--grid-div-num: ${size}`);
+        .querySelector("*")
+        .setAttribute("style", `--grid-div-num: ${size}`);
     // addGridDivCounter(gridDivs); //If I want to count the squares on the screen
+}
+function getRandomColor() {
+    return `#${Math.floor(Math.random() * 16 ** 6).toString(16)}`;
 }
 function addGridDivCounter(gridDivs) {
     let gridDivNumX = 0;
@@ -25,7 +28,7 @@ function addGridDivCounter(gridDivs) {
         }
     });
 }
-function addListenerGridDivNumber() {
+function addListenerChooseGridDivNumber() {
     chooseNumberButton.addEventListener("click", () => {
         const rawUserInput = prompt("Enter a number between 1 and 100:", 16);
         const userInputAsNumber = Number(rawUserInput);
@@ -33,9 +36,21 @@ function addListenerGridDivNumber() {
             userInputAsNumber >= 1 && userInputAsNumber <= 100
                 ? Math.floor(userInputAsNumber)
                 : 16;
-        drawSketch(numberOfGridDivs)
+        drawSketch(numberOfGridDivs);
     });
 }
-drawSketch();
-addListenerGridDivNumber()
+// I try to change the color with every "mouseover". Currently, It fails, and I dont necessarily know why.
+// Only happens when I change a style attribute using the CSS variable...
+function addListenerChangeColorOnHover() {
+    containerGridDiv.addEventListener("mouseover", () => {
 
+        console.log("touched")
+    }); 
+}
+drawSketch();
+addListenerChooseGridDivNumber();
+addListenerChangeColorOnHover();
+
+// JS / CSS
+// - random color when hover over div
+// - each interaction => +10% darkening effect on div UNTIL 100% reached (css opacity?)
